cdb8a32a05468e9bf6af7b1a1489c9eb890b8371
Jesus Anaya <jesus.anaya.developer@gmail.com>
2023-07-02 18:31:10 -0600

    se crean componentes

---
+++ src/app/components/buscar-imagen/buscar-imagen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-imagen',
+  templateUrl: './buscar-imagen.component.html',
+  styleUrls: ['./buscar-imagen.component.css']
+})
+export class BuscarImagenComponent {
+
+}
